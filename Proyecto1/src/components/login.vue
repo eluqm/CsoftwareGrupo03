89536e6def540ef21fd90b7e50e7c9b39de8d24c
Olger <oquispev@ulasalle.edu.pe>
2023-10-02 21:38:07 -0500

    Agregando partes login

--- Proyecto1/src/components/login.vue
+++ Proyecto1/src/components/login.vue
@@ -1,7 +1,34 @@
 <template>
+    <div>
+        <div>
+            <img src="" alt="">
+            <h1> 
+                Institucion Mariano Melgar
+            </h1>
+        </div>
+        <form action="">
+            <div>
+                <label for="dni"> 
+                    DNI:
+                </label>
+                <input type="number" name="" id="" required>
+            </div>
+            <div>
+                <label for="password">Contraseña:</label>
+                <input type="password">
+            </div>
+            <div>
+                <button type="submit">
+                    Ingresar
+                </button>
+            </div>
+            <div>
+                <a href="#"> ¿Olvidaste tu contraseña? </a>
+            </div>
 
+        </form>
+    </div>
+    <div>
+
+    </div>
 </template>
-
-<script>
-
-</script>
