715720b16db9b12f77c70949775e20fbc10da8f1
Olger <oquispev@ulasalle.edu.pe>
2023-10-10 18:43:35 -0500

    Datos del Login

--- Proyecto1/Front/main.js
+++ Proyecto1/Front/main.js
@@ -2,6 +2,7 @@ const usuario = document.getElementById('dni')
 const password = document.getElementById('password')
 const boton = document.getElementById('boton')
 
+//ya tengo los datos del login
 boton.addEventListener('click',(e) => {
     e.preventDefault()
     const data = {
