1d95072676f631cb49d11eb6d833754abcb0d1b3
Olger <oquispev@ulasalle.edu.pe>
2023-10-11 08:33:25 -0500

    Funciobes Apoderado

--- Proyecto1/Front/main.js
+++ Proyecto1/Front/main.js
@@ -1,13 +1,20 @@
-const usuario = document.getElementById('dni')
-const password = document.getElementById('password')
-const boton = document.getElementById('boton')
+function validateLogin() {
+    var username = document.getElementById("dni").value;
+    var password = document.getElementById("password").value;
+    var message = document.getElementById("message");
 
-//ya tengo los datos del login
-boton.addEventListener('click',(e) => {
-    e.preventDefault()
-    const data = {
-        usuario: usuario.value,
-        password:password.value
+    /// Verificar si los campos están vacíos
+    if (username === '' || password === '') {
+        message.textContent = 'Por favor, complete todos los campos.';
+    } else {
+        // Simula una validación exitosa. Aquí puedes agregar tu lógica de autenticación real.
+        var isAuthenticated = true;
+
+        if (isAuthenticated) {
+            // Redirige al usuario a otra página después de una validación exitosa
+            window.location.href = "./ApoderadoInicio.html";
+        } else {
+            message.textContent = 'Credenciales incorrectas. Por favor, inténtelo de nuevo.';
+        }
     }
-    console.log(data)
-})
+}
