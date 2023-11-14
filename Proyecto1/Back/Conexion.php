c4cad6c73b72f99579f4eae22067af2da93ab0b4
Olger <oquispev@ulasalle.edu.pe>
2023-11-14 07:55:50 -0500

    Agregando Error conexion BD

--- Proyecto1/Back/Conexion.php
+++ Proyecto1/Back/Conexion.php
@@ -2,6 +2,13 @@
 
 //select alumno_dni,clave from alumno where alumno_dni='12345678' and clave = '123456';
 
-$conexion = pg_connect("host=localhost dbname=dbphp user=postgres password=browen2001")
+$conexion = pg_connect("host=localhost dbname=dbphp user=postgres password=browen2001");
+
+if (!$conexion) {
+    $error = error_get_last();
+    echo "Error de conexión: " . $error['message'];
+    // O podrías intentar esto si estás usando PHP 7 o superior
+    throw new Exception(pg_last_error());
+}
 
 ?>
